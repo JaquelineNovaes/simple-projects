4be72b7073114c90a63b6c5a4742f7e161a1b08c
Jaqueline Novaes <98557538+JaquelineNovaes@users.noreply.github.com>
2022-02-05 14:21:03 -0300

    Add files via upload

---
+++ Urna/Urna/Candidato.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urna
+{
+    class Candidato
+    {
+        public string Numero { get; set; }
+        public string Nome { get; set; }
+        public string Chapa { get; set; }
+        public string Imagem { get; set; }
+        public int Voto { get; set; }
+
+        public Candidato(string numero, string nome, string chapa, string imagem, int voto)
+        {
+            this.Numero = numero;
+            this.Nome = nome;
+            this.Chapa = chapa;
+            this.Imagem = imagem;
+            this.Voto = voto; 
+        }
+    }
+}
